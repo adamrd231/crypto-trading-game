b7d64a6317b217801484a3807a7ea83b63cb2409
adamrd231 <adam.rdconcepts@gmail.com>
2022-01-08 21:39:02 -0700

    Initial Commit

---
+++ nftApp/nftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  nftAppApp.swift
+//  nftApp
+//
+//  Created by Adam Reed on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct nftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
